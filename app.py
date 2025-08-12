1eb47c7a565298b6956c033ad6da9df937e4c92f
Mohit Agarwalla <142979857+MoH1t1@users.noreply.github.com>
2025-08-12 19:12:19 +0530

    Update and rename application.py to app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
 
 
 
